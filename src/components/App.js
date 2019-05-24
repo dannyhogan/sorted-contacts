7b6ec32fecaba0f4e44e1bc0b6b055398f1f59be
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-24 13:56:13 -0700

    Created app component and exported to index.js

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import Component from './Component.js';
+
+class App extends Component {
+    render() {
+        const dom = this.renderDOM();
+
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <main></main>
+            </div>
+        `;
+    }
+}
+
+export default App;
